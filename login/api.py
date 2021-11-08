a24e1ef8d14f669879250527596d334b3045c3f6
Ubuntu <pomchi@gcloudapi.xxgg3c1plcvunkhcznugcannsa.ix.internal.cloudapp.net>
2021-11-08 03:26:57 +0000

    修正遺失login函數

--- login/api.py
+++ login/api.py
@@ -1,10 +1,43 @@
 import logging
 from flask import Blueprint,request
 from model import DB_CONNECTOR,FACE_COMPAROR_DICT
-from utils import get_account_collection,make_result_msg
+from utils import get_account_collection,make_result_msg,reload_feature
 from schema import request_to_dict,collection_schema_dict
 
 login_app = Blueprint('login', __name__)
+
+
+@login_app.route('/login',methods = ['POST'])
+def login():
+    """
+    input:
+        'account' : company account's name.
+        'password' : company account's passowrd.
+
+    output:
+        result_string:
+            if account and password conform to DB : 
+                status : True
+            else:
+                status : False
+    """
+    logging.info('Call login.')
+    if request.method == 'POST':
+        result_string = _login(request.get_json())
+        return result_string
+    else:
+        return make_result_msg(False,error_msg='REQUEST FAILED')
+
+def _login(values):
+    global DB_CONNECTOR,FACE_COMPAROR_DICT
+    login_info = request_to_dict(values,collection_schema_dict['login'])
+    the_same_docs = DB_CONNECTOR.query_data('accounts',login_info,{})
+    
+    if the_same_docs:
+        reload_feature(login_info['account'])
+        return make_result_msg(True)
+    else:
+        return make_result_msg(False)
 
 
 @login_app.route('/get_login_user',methods = ['POST'])
