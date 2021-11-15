83eb27a55112172b7fbadfb13cb1155557a61a78
Ubuntu <pomchi@gcloudapi.xxgg3c1plcvunkhcznugcannsa.ix.internal.cloudapp.net>
2021-11-15 03:08:49 +0000

    回傳工時時，增加wage欄位

--- record/model.py
+++ record/model.py
@@ -1,31 +1,40 @@
 import gridfs
-from utils import make_result_msg,get_account_collection
-from schema import request_to_dict,collection_schema_dict
+from utils import make_result_msg, get_account_collection
+from schema import request_to_dict, collection_schema_dict
 from model import DB_CONNECTOR
 from datetime import datetime
 
+
 def _cal_working_hours(values):
-    
-    account,workhour_dict = request_to_dict(values,collection_schema_dict['workhour'],is_include_account=True)
+
+    account, workhour_dict = request_to_dict(
+        values, collection_schema_dict['workhour'], is_include_account=True)
     this_account_collection = get_account_collection(account)
-    starttime = datetime.strptime(workhour_dict['starttime'],"%Y/%m/%d")
-    endtime = datetime.strptime(workhour_dict['endtime'],"%Y/%m/%d")
+    starttime = datetime.strptime(workhour_dict['starttime'], "%Y/%m/%d")
+    endtime = datetime.strptime(workhour_dict['endtime'], "%Y/%m/%d")
     expression_list = []
-    expression_list.append({"$project":{"status":"$status","_id":"$_id","userid":"$userid","date":{"$dateFromString":{"dateString":"$date"}}}})
-    expression_list.append({"$match":{"date":{"$gte":starttime,"$lt":endtime}}})
-    expression_list.append({"$group" : {"_id" : "$userid" ,"result_list" : {"$push" : {"status" : "$status","date" : "$date"}}}})
-    expression_list.append({"$sort" : {"result_list.date" : 1}})
-    expression_list.append({"$lookup" : {"from" : this_account_collection['user'] ,"localField" : "_id" ,"foreignField" : "_id" ,"as" : "user"}})
-    result_data = DB_CONNECTOR.aggregate(this_account_collection['clockin'],expression_list)
+    expression_list.append({"$project": {"status": "$status", "_id": "$_id",
+                           "userid": "$userid", "date": {"$dateFromString": {"dateString": "$date"}}}})
+    expression_list.append(
+        {"$match": {"date": {"$gte": starttime, "$lt": endtime}}})
+    expression_list.append({"$group": {"_id": "$userid", "result_list": {
+                           "$push": {"status": "$status", "date": "$date"}}}})
+    expression_list.append({"$sort": {"result_list.date": 1}})
+    expression_list.append({"$lookup": {
+                           "from": this_account_collection['user'], "localField": "_id", "foreignField": "_id", "as": "user"}})
+    result_data = DB_CONNECTOR.aggregate(
+        this_account_collection['clockin'], expression_list)
 
     user_hour_dict = {}
     for user_clockin_doc in result_data:
-        username = user_clockin_doc['user'][0]['name']
+        user_name = user_clockin_doc['user'][0]['name']
+        user_wage = user_clockin_doc['user'][0]['wage']
         work_hours = cal_work_hours(user_clockin_doc['result_list'])
-        user_hour_dict[username] = work_hours
+        user_hour_dict[user_name] = {'hours': work_hours, 'wage': user_wage}
 
-    return make_result_msg(True,None,user_hour_dict)
-    
+    return make_result_msg(True, None, user_hour_dict)
+
+
 def cal_work_hours(clockin_dict):
     Clock_On = None
     ClockOn_time = 0
@@ -41,22 +50,26 @@ def cal_work_hours(clockin_dict):
             start_time = ClockOn_time
             total_hours = ((end_time - start_time).seconds) + total_hours
             Clock_On = False
-    return total_hours // 3600     
+    return total_hours // 3600
 
 
 def _get_user_record(values):
 
     account = values['account']
     this_account_collection = get_account_collection(account)
-    
-    expression_list = []
-    expression_list.append({"$lookup":{"from":this_account_collection['user'],"localField":"userid","foreignField":"_id","as":"user"}})
-    expression_list.append({"$lookup":{"from":this_account_collection['record'],"localField":"recordID","foreignField":"_id","as":"image"}})
-    expression_list.append({"$project":{"date":1,"status":1,"user.name":1,"image.cropimageID":1}})
-    result_data = DB_CONNECTOR.aggregate(this_account_collection['clockin'],expression_list)
 
-    fs = gridfs.GridFS(DB_CONNECTOR.db,this_account_collection['image'])
-    
+    expression_list = []
+    expression_list.append({"$lookup": {
+                           "from": this_account_collection['user'], "localField": "userid", "foreignField": "_id", "as": "user"}})
+    expression_list.append({"$lookup": {
+                           "from": this_account_collection['record'], "localField": "recordID", "foreignField": "_id", "as": "image"}})
+    expression_list.append({"$project": {
+                           "date": 1, "status": 1, "user.name": 1, "image.cropimageID": 1}})
+    result_data = DB_CONNECTOR.aggregate(
+        this_account_collection['clockin'], expression_list)
+
+    fs = gridfs.GridFS(DB_CONNECTOR.db, this_account_collection['image'])
+
     output = []
     for doc in result_data:
         del doc['_id']
@@ -66,5 +79,4 @@ def _get_user_record(values):
         doc['image'] = raw_image
         output.append(doc)
 
-
-    return make_result_msg(True,result=output)
+    return make_result_msg(True, result=output)
