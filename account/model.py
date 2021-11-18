dd960f4e133cf32ac6ab9592b7d11e29f2481bcc
Ubuntu <pomchi@gcloudapi.xxgg3c1plcvunkhcznugcannsa.ix.internal.cloudapp.net>
2021-11-18 09:37:17 +0000

    merge account and user collection

--- account/model.py
+++ account/model.py
@@ -11,25 +11,16 @@ def _company_register(values):
     account_dict = request_to_dict(values, collection_schema_dict['account'])
     account = Account(account_dict)
     the_same_docs = DB_CONNECTOR.query_data(
-        'accounts', {'account': account.data['account']}, {'account': 1})
+        'profile', {'account': account.data['account']}, {'account': 1})
 
     account.data['password'] = generate_password_hash(account.data['password'])
-
+    account.data['users'] = {}
     if the_same_docs is None:
 
         account_id = DB_CONNECTOR.insert_data('accounts', account.data)
         account_name = account.data['account']
-
         logging.info('Create Accont : %s , InsertID : %s' %
                      (account_name, str(account_id)))
-
-        DB_CONNECTOR.create_collection('user_%s' % (account_name))
-        DB_CONNECTOR.create_collection('eigenvalue_%s' % (account_name))
-        DB_CONNECTOR.create_collection('record_%s' % (account_name))
-        DB_CONNECTOR.create_collection('image_%s.files' % (account_name))
-        DB_CONNECTOR.create_collection('image_%s.chunks' % (account_name))
-        DB_CONNECTOR.create_collection('clockin_%s' % (account_name))
-
         return make_result_msg(True)
     else:
         logging.warning('front-end POST the same account in DB.')
@@ -40,12 +31,9 @@ def _remove_company_account(values):
     account = values['account']
     this_account_collection = get_account_collection(account)
     if check_account_exist(account):
-        DB_CONNECTOR.delete_data('accounts', {'account': account})
-        logging.info('remove %s account' % (account))
-        for collection_type in this_account_collection.keys():
-            # print('kill ', this_account_collection[collection_type])
-            DB_CONNECTOR.drop_collection(
-                this_account_collection[collection_type])
+        DB_CONNECTOR.delete_data('profile', {'account': account})
+        DB_CONNECTOR.delete_data('eigenvalue', {'account': account})
+        DB_CONNECTOR.delete_data('record', {'account': account})
 
         return make_result_msg(True)
     else:
