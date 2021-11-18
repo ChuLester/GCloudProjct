92d706ec272d3c0dafb138602e7999c0b302cf28
Ubuntu <pomchi@gcloudapi.xxgg3c1plcvunkhcznugcannsa.ix.internal.cloudapp.net>
2021-11-18 09:32:56 +0000

    更改連線資料庫

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 MongoConfig = {
     'host': "mongodb://gclouddb:ejQ19HnvVeiQFcaFIZ4oRoc5zyQS63wUF57wPW1UOxMrTtB1fY8B5zrMWiW5l4eXXkTIXcAKocnVWSGnXUZSCg==@gclouddb.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@gclouddb@",
-    'cloud_face_db':'cloudfaceapp'
+    'cloud_face_db': 'cloudfaceappv2'
 }
 
 InferenceConfig = {
-    'host':'127.0.0.1',
-    'port':'9001',
-    'model_name':'lightcnn'
+    'host': '127.0.0.1',
+    'port': '9001',
+    'model_name': 'lightcnn'
 }
 
 RecognizeConfig = {
-    'threshold' : 0.3
+    'threshold': 0.3
 }
 
 PayConfig = {
-    'wage':168
+    'wage': 168
 }
