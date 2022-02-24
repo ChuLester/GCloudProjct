50080def1d29f578fdff3b6f51658b792fa6337e
lester <dxxlin2065@gmail.com>
2022-02-24 10:23:38 +0800

    新增api:manual_update_record

--- record/api.py
+++ record/api.py
@@ -1,7 +1,7 @@
 import logging
 from flask import Flask, request, Blueprint
 from utils import make_result_msg
-from record.model import _cal_working_hours, _get_user_record, _edit_user_record
+from record.model import _cal_working_hours, _get_user_record, _edit_user_record, _manual_update_record
 from error_code import error_code_dict
 record_app = Blueprint('record', __name__)
 
@@ -81,6 +81,25 @@ def edit_user_record():
     logging.info('Call edit_user_record.')
     if request.method == 'POST':
         result_string = _edit_user_record(request.get_json())
+        return result_string
+    else:
+        return make_result_msg(False, error_msg=error_code_dict[600])
+
+
+@record_app.route('/manual_update_record', methods=['POST'])
+def manual_update_record():
+    """
+    input:
+        account : company account's name
+        record_object_id : record object id.
+        user_object_id : user object id.
+    output:
+        True,False
+
+    """
+    logging.info('Call manual_update_record.')
+    if request.method == 'POST':
+        result_string = _manual_update_record(request.get_json())
         return result_string
     else:
         return make_result_msg(False, error_msg=error_code_dict[600])
